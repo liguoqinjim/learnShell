9add70b51db750dcf58f3f095f31f456fbb8720a
liguoqinjim <liguoqinjim23@gmail.com>
2015-02-25 21:31:57 -0800

    xphone/create_global_pvp.sh

---
+++ xphone/create_global_pvp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+user="root"
+passwd="1234"
+tmp_file="table.sql"
+host="127.0.0.1"
+port=3306
+
+
+create_global_user_pvp_table_sql(){
+cat << EOF > $tmp_file
+	CREATE TABLE t_global_pvp(
+		userid int unsigned not null,
+		pvp_value int unsigned not null,
+		pvp_point int unsigned not null,
+		primary key (userid)
+	)ENGINE=innodb CHARSET=UTF8;
+EOF
+}
+
+
+create_global_user_pvp_table_sql
+cat $tmp_file | mysql -u root -p$passwd -h $host -P $port "test"
--- xphone/table.sql
+++ xphone/table.sql
@@ -1,9 +1,6 @@
-	#9是传进来的第一个参数
-	CREATE TABLE t_user_soul_9(
+	CREATE TABLE t_global_pvp(
 		userid int unsigned not null,
-		soulid int unsigned not null,
-		skill_level int unsigned not null default 1,
-		whether char(1) not null default '0', 
-		attach_attr binary(32) not null default 0x00000000,
-		primary key (userid,soulid)
+		pvp_value int unsigned not null,
+		pvp_point int unsigned not null,
+		primary key (userid)
 	)ENGINE=innodb CHARSET=UTF8;
