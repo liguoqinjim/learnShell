ac9e7578c3dba06653eddb38871852c3bc8a1c5d
liguoqinjim <liguoqinjim23@gmail.com>
2015-03-09 02:39:01 -0700

    xphone_create_global_user_pvp

--- xphone/create_global_user_pvp.sh
+++ xphone/create_global_user_pvp.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+user="root"
+passwd="1234"
+tmp_file="table.sql"
+host="127.0.0.1"
+port=3306
+
+
+create_global_user_pvp_table_sql(){
+cat << EOF > $tmp_file
 	CREATE TABLE t_global_user_pvp(
 		userid int unsigned not null,
 		pvp_value int unsigned not null,
@@ -7,3 +18,9 @@
 		pvp_value_temp int unsigned not null,
 		primary key (userid)
 	)ENGINE=innodb CHARSET=UTF8;
+EOF
+}
+
+
+create_global_user_pvp_table_sql
+cat $tmp_file | mysql -u root -p$passwd -h $host -P $port "test"
